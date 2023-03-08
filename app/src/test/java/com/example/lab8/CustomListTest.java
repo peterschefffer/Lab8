10ad34a36eafbfeedff10f5d39c2d0179c67bdfd
peterschefffer <pscheff8@gmail.com>
2023-03-07 17:40:20 -0700

    RED: add test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> cityList = new ArrayList<>();
 
+    @Before
+    public void init() {
+        cityList = new ArrayList<>();
+    }
 
+    @Test
+    public void testAddCity() {
+        CustomList list = new CustomList(null, cityList);
+        City city = new City("Edmonton", "Alberta");
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+    }
 }
